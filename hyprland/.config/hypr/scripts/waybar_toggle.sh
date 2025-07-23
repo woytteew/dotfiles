80fc61da09855381c94276a655a2ca42017f1aab
Vojtech Czakan <vojtech.czakan@gmail.com>
2025-07-23 14:27:02 +0200

    hyprland: startup execute waybar_toggle.sh

---
+++ hyprland/.config/hypr/scripts/waybar_toggle.sh
@@ -0,0 +1,18 @@
+last_ws=1
+
+while true; do
+	ws=$(hyprctl activeworkspace -j | jq '.id')
+	if [ "$ws" != "$last_ws" ]; then
+		if [ "$ws" -ge 1 ] && [ "$ws" -le 4 ]; then
+			if { [ "$last_ws" -ge 5 ] && [ "$last_ws" -le 9 ]; } || [ "$last_ws" -eq 0 ]; then
+				pkill -SIGUSR1 waybar # hide waybar
+			fi
+		elif { [ "$ws" -ge 5 ] && [ "$ws" -le 9 ]; } || [ "$ws" -eq 0 ]; then
+			if [ "$last_ws" -ge 1 ] && [ "$last_ws" -le 4 ]; then
+				pkill -SIGUSR2 waybar # show
+			fi
+		fi
+		last_ws="$ws"
+	fi
+	sleep 0.2
+done
